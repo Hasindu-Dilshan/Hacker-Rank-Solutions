10d5cdc48a24f672b220250cd3ce592d10167bb2
HasinduDilshan10 <hasindudilshan10@gmail.com>
2023-03-13 06:26:57 +0530

    Previously answered quests

---
+++ Preparation Challenges/Simple Algorithms/DrawingBook.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+    public static int pageCount(int n, int p) {
+    // Write your code here
+        if(p <= n - p) {
+            if(p == 1) {
+                return 0;
+            }
+            else {
+                return p / 2;
+            }
+        }
+        else if(n % 2 == 1) {
+            if(p == n){
+                return 0;
+            }
+            else {
+                return (n - p) / 2;
+            }
+        }
+        else{
+            if(p == n){
+                return 0;
+            }
+            else {
+                return (n - p + 1) / 2;
+            }
+        }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ Preparation Challenges/Simple Algorithms/SalesByMatch.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+    // Write your code here
+    
+        List<Integer> matchingSockCount = new ArrayList<Integer>();
+        int pairCount = 0;
+        
+        // Initialize matchingSockCount
+        for(int i = 0; i < 100; i++) {
+            matchingSockCount.add(0);
+        }
+        
+        for(Integer sockNum: ar) {
+            int index = sockNum - 1;
+            matchingSockCount.set(index, matchingSockCount.get(index) + 1);
+        }
+        
+        for(Integer sockCount: matchingSockCount) {
+            pairCount += sockCount / 2;
+        }
+        
+        return pairCount;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> ar = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arTemp[i]);
+            ar.add(arItem);
+        }
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
